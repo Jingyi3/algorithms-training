42d4a9e77cb13268d3383fb7ef671749d8f33bf7
Jingyi3 <oliz0991@gmail.com>
2019-09-23 13:41:02 -0400

    Q027,Q032 solution

---
+++ src/com/olivia/leetcode/Q032_LongestValidParentheses.java
@@ -0,0 +1,45 @@
+package com.olivia.leetcode;
+
+import java.util.Stack;
+
+/**
+ *Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+ *
+ * Example 1:
+ *
+ * Input: "(()"
+ * Output: 2
+ * Explanation: The longest valid parentheses substring is "()"
+ * Example 2:
+ *
+ * Input: ")()())"
+ * Output: 4
+ * Explanation: The longest valid parentheses substring is "()()"
+ */
+public class Q032_LongestValidParentheses {
+    public int longestValidParentheses(String s) {
+        if (s == null || s.length() < 2) return 0;
+        int n = s.length();
+        int max = 0;
+        int leftmost = -1;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else{
+                if (stack.isEmpty()) {
+                    leftmost = i;
+                } else {
+                    int j = stack.pop();
+                    if(stack.isEmpty())
+                        max = Math.max(max, i - leftmost);
+                    else
+                        max = Math.max(max, i - stack.peek());
+
+                }
+            }
+        }
+        return max;
+    }
+
+}
