4ca1f077f48d325d079162d2b7535fd2f190b1d0
Jingyi3 <oliz0991@gmail.com>
2019-09-25 01:28:51 -0400

    IMPORTANT THINGS in binary search template

--- src/com/olivia/basic/Demo04_BinarySearch.java
+++ src/com/olivia/basic/Demo04_BinarySearch.java
@@ -9,12 +9,13 @@ public class Demo04_BinarySearch {
         if (nums == null || nums.length == 0) return 0;
         int start = 0;
         int end = nums.length - 1;
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] < target) start = mid;
+        while (start + 1 < end) {//当两个指针相遇的时候
+            int mid = start + (end - start) / 2;//防止overflow（如果直接相加/2）
+            if (nums[mid] < target) start = mid;//移动指针的时候直接=mid而不是mid+1 或者mid-1
             else if (nums[mid] > target) end = mid;
             else return mid;
         }
+        //while循环结束后 根据题意，一次检测，在剩下的这两个start和end位置上对应的数，返回搜索的结果
         if (nums[end] < target) return end + 1;
         else if (nums[start] >= target) return start;
         else return end;
