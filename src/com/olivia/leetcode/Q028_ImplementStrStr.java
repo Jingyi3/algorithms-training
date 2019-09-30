99414770ac5b88f94c498284949b87c271519ce8
Jingyi3 <oliz0991@gmail.com>
2019-09-29 23:59:39 -0400

    Q28

---
+++ src/com/olivia/leetcode/Q028_ImplementStrStr.java
@@ -0,0 +1,40 @@
+package com.olivia.leetcode;
+
+/**
+ *
+ * Implement strStr().
+ *
+ * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ *
+ * Example 1:
+ *
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ * Example 2:
+ *
+ * Input: haystack = "aaaaa", needle = "bba"
+ * Output: -1
+ * Clarification:
+ *
+ * What should we return when needle is an empty string? This is a great question to ask during an interview.
+ *
+ * For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+ *
+ *
+ */
+public class Q028_ImplementStrStr {
+    public int strStr(String haystack, String needle) {
+        if(needle==null||needle.length()==0) return 0;
+        if (haystack==null||haystack.length()==0) return -1;
+
+        int n = haystack.length();
+        int m = needle.length();
+        for (int i = 0; i < n - m + 1; i++) {
+            for (int j = 0; j < m; j++) {
+                if(haystack.charAt(i+j)!=needle.charAt(j)) break;
+                if(j==m-1) return i;
+            }
+        }
+        return -1;
+    }
+}
